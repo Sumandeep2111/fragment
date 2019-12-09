80a8d83050e5393867798b50c99f605109f4d83e
Sumandeep <sumandeep21111994@gmail.com>
2019-12-09 16:52:57 -0500

    list fragment onclick

--- app/src/main/java/com/example/fragmentdemo/PositionTitleFragment.java
+++ app/src/main/java/com/example/fragmentdemo/PositionTitleFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 /**
@@ -34,4 +35,8 @@ public class PositionTitleFragment extends ListFragment {
       return super.onCreateView(inflater,container,savedInstanceState);
     }
 
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+    }
 }
