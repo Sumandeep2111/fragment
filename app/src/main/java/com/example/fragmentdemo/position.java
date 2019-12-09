1d30168fe4378a2af5191b00b9ffa2e25e4a0222
Sumandeep <sumandeep21111994@gmail.com>
2019-12-09 16:14:19 -0500

    fragment

---
+++ app/src/main/java/com/example/fragmentdemo/position.java
@@ -0,0 +1,21 @@
+package com.example.fragmentdemo;
+
+import android.icu.text.Transliterator;
+
+public class position {
+
+    private String title;
+    private String description;
+
+    public position(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public  static final position[] position = {
+            new position("ios developer","developing ios application"),
+            new position("java developer","developing java application"),
+            new position("database developer","developing database application")
+    };
+
+}
